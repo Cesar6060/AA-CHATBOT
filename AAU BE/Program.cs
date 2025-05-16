e02efabe0095a0dec1704fbe0d99ab3e1821d268
Cesar V <cesarvillarreal11@gmail.com>
2025-05-15 20:49:53 -0400

    Initial commit of AAU Chatbot project

--- AAU BE/Program.cs
+++ AAU BE/Program.cs
@@ -33,13 +33,14 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Load environment variables
+
 DotNetEnv.Env.Load();
 
-// Add services to the container.
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 
-// Add CORS policy
+// CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
@@ -67,10 +68,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- //ONLY ENABLE THESE TO RUN UNIT TESTS 
-    //var test = new Test();
-    //test.Test_AuthenticateUser_WithInValidCredentials();
-    //test.Test_AddUser_DeleteUser_All_User_Functions();
+//ONLY ENABLE THESE TO RUN UNIT TESTS 
+//var test = new Test();
+//test.Test_AuthenticateUser_WithInValidCredentials();
+//test.Test_AddUser_DeleteUser_All_User_Functions();
 ///Console.WriteLine("Tests executed.");
 
 app.UseHttpsRedirection();
